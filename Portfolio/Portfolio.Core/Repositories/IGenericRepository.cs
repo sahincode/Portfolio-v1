9ae47ff679324a80c183e511111d3d067564ce55
sahincode <ismayilovsahin714@gmail.com>
2024-06-09 01:31:04 +0400

    Generic repository created.

---
+++ Portfolio/Portfolio.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Core.Repositories
+{
+    public interface IGenericRepository<T> where T :BaseEntity , new()
+    {
+        public DbSet<T> Table { get; }
+        public Task CreateAsync(T entity);
+        public void Delete(T entity);
+        public Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? expression, params string[]? includes);
+        public Task<T> Get(Expression<Func<T, bool>>? expression, params string[]? includes);
+        public Task<int> CommitAsync();
+    }
+}
---
+++ Portfolio/Portfolio.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Core.Entities;
+using Portfolio.Core.Repositories;
+using Portfolio.Data.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T:BaseEntity ,new()
+    {
+        private  readonly PortfolioDbContext _context;
+       
+        public GenericRepository(PortfolioDbContext context)
+        {
+            this._context = context;
+        }
+        public DbSet<T> Table => _context.Set<T>();
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+            
+        }
+
+        public  async Task CreateAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            Table.Remove(entity);
+        }
+
+        public async  Task<T> Get(Expression<Func<T, bool>>? expression, params string[]? includes)
+        {
+            var query = this.GetQuery(includes);
+            return expression is not null ? await query.Where(expression).FirstOrDefaultAsync() : await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? expression, params string[]? includes)
+        {
+            var query = this.GetQuery(includes);
+            return expression is not null ? await query.Where(expression).ToListAsync() : await query.ToListAsync();
+        }
+        public IQueryable<T> GetQuery(params string[] includes)
+        {
+            var query = Table.AsQueryable();
+            if (includes is not null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+            return query;
+        }
+    }
+}
