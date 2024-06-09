b3e8dec05229935e15f4ced0af80802c4610f3e6
sahincode <ismayilovsahin714@gmail.com>
2024-06-09 23:27:41 +0400

    Employee repository created.

---
+++ Portfolio/Portfolio.Core/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿using Portfolio.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Core.Repositories
+{
+    public interface IEmployeeRepository:IGenericRepository<Employee>{}
+}
---
+++ Portfolio/Portfolio.Data/Repositories/EmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Portfolio.Core.Entities;
+using Portfolio.Core.Repositories;
+using Portfolio.Data.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Data.Repositories
+{
+    public class EmployeeRepository : GenericRepository<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(PortfolioDbContext context) : base(context){}
+    }
+}
