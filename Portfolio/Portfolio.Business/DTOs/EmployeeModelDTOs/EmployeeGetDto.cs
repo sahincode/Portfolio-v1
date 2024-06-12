5f373e9cfb3d2282921674a8197b579386f47f2b
sahincode <ismayilovsahin714@gmail.com>
2024-06-12 12:28:49 +0400

    EmployeeUpdateDto and EmployeeGetDto created.

--- Portfolio/Portfolio.Business/DTOs/EmployeeModelDTOs/EmployeeGetDto.cs
+++ Portfolio/Portfolio.Business/DTOs/EmployeeModelDTOs/EmployeeGetDto.cs
@@ -9,5 +9,16 @@ namespace Portfolio.Business.DTOs.EmployeeModelDTOs
 {
     public class EmployeeGetDto
     {
+        public  string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Experience { get; set; }
+        public List<string> Professions { get; set; }
+        public string ImageUrl { get; set; }
+        public string LinkedIn { get; set; }
+        public string Insta { get; set; }
+        public string Facebook { get; set; }
+        public string GitHub { get; set; }
     }
 }
--- Portfolio/Portfolio.Business/DTOs/EmployeeModelDTOs/EmployeeUpdateDto.cs
+++ Portfolio/Portfolio.Business/DTOs/EmployeeModelDTOs/EmployeeUpdateDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,51 @@ namespace Portfolio.Business.DTOs.EmployeeModelDTOs
 {
     public class EmployeeUpdateDto
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Experience { get; set; }
+        public List<string> Professions { get; set; }
+        public string ImageUrl { get; set; }
+        public string LinkedIn { get; set; }
+        public string Insta { get; set; }
+        public string Facebook { get; set; }
+        public string GitHub { get; set; }
+    }
+    public class EmployeeUpdateDtoValidator : AbstractValidator<EmployeeUpdateDto>
+    {
+        public EmployeeUpdateDtoValidator()
+        {
+
+            RuleFor(e => e.Name).NotNull().WithMessage("Can not be null").
+                                 NotEmpty().WithMessage("Can not be empty").
+                                 MaximumLength(200).WithMessage("Can not be greater than 200 digits").
+                                 MinimumLength(3).WithMessage("Can not be less than 3 digits");
+            RuleFor(e => e.Surname).NotNull().WithMessage("Can not be null").
+                                 NotEmpty().WithMessage("Can not be empty").
+                                 MaximumLength(200).WithMessage("Can not be greater than 200 digits").
+                                 MinimumLength(3).WithMessage("Can not be less than 3 digits");
+            RuleFor(e => e.ImageUrl).NotNull().WithMessage("Can not be null").
+                                 NotEmpty().WithMessage("Can not be empty").
+                                 MaximumLength(100).WithMessage("Can not be greater than 100 digits").
+                                 MinimumLength(3).WithMessage("Can not be less than 3 digits");
+            RuleFor(e => e.Insta).NotNull().WithMessage("Can not be null").
+                                 NotEmpty().WithMessage("Can not be empty").
+                                 MaximumLength(200).WithMessage("Can not be greater than 200 digits").
+                                 MinimumLength(3).WithMessage("Can not be less than 3 digits");
+            RuleFor(e => e.LinkedIn).NotNull().WithMessage("Can not be null").
+                                 NotEmpty().WithMessage("Can not be empty").
+                                 MaximumLength(200).WithMessage("Can not be greater than 200 digits").
+                                 MinimumLength(3).WithMessage("Can not be less than 3 digits");
+            RuleFor(e => e.Facebook).NotNull().WithMessage("Can not be null").
+                                 NotEmpty().WithMessage("Can not be empty").
+                                 MaximumLength(200).WithMessage("Can not be greater than 200 digits").
+                                 MinimumLength(3).WithMessage("Can not be less than 3 digits");
+            RuleFor(e => e.GitHub).NotNull().WithMessage("Can not be null").
+                                 NotEmpty().WithMessage("Can not be empty").
+                                 MaximumLength(200).WithMessage("Can not be greater than 200 digits").
+                                 MinimumLength(3).WithMessage("Can not be less than 3 digits");
+        }
     }
 }
